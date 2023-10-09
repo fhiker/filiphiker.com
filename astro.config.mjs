26ccfa80f355407b6ca120fc04b52d0635e0122e
Filip Hiker <filiphiker@Davids-MacBook-Pro.local>
2023-10-09 22:15:01 +0200

    try fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://fhiker.github.io',
-  base: '/filiphiker.com',
+  base: '/',
 });
